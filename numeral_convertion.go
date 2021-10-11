25b206fb2fca82c6a568616a91ff48c0b8dd8b6a
iamargus95 <kamathsuraj95@gmail.com>
2021-10-11 21:41:14 +0530

    Added flags to choose between roman and arabic conversions

--- numeral_convertion.go
+++ numeral_convertion.go
@@ -7,11 +7,26 @@ import (
 )
 
 func main() {
+
+	var roman bool
+	flag.BoolVar(&roman, "r", false, "Does a roman numeral to indo-arabic numeral conversion.")
+
+	var arabic bool
+	flag.BoolVar(&arabic, "a", false, "Does a indo-arabic numeral to roman numeral conversion.")
+
 	flag.Parse()
-	arabic := roman2Arabic(flag.Arg(0))
-	fmt.Println(arabic)
-	roman := arabic2Roman(flag.Arg(1))
-	fmt.Println(roman)
+
+	if roman {
+
+		result := roman2Arabic(flag.Arg(0))
+		fmt.Println(result)
+	}
+
+	if arabic {
+
+		result := arabic2Roman(flag.Arg(0))
+		fmt.Println(result)
+	}
 }
 
 func arabic2Roman(input string) string {
@@ -51,7 +66,6 @@ func arabic2Roman(input string) string {
 func roman2Arabic(input string) string {
 
 	romanToArabic := map[string]int{"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-
 	roman := []byte(input)
 
 	big := 1000
